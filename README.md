c277c37a9693de6353387df609d02008529e09a5
schizzy <150442422+Scissors-LT@users.noreply.github.com>
2025-08-30 05:19:11 +0100

    added support for custom hotkeys
    I was high as fuck and did this at 4 am because i couldnt sleep, i think it works

--- README.md
+++ README.md
@@ -4,9 +4,7 @@
 
 uhhh you just press f6 and then it scrolls down really fast until you press f6 again...
 
-
-if i get like 3 stars on this repo ill let you change the hotkey or something in the window idk man
-...or just add it yourself ig, the source is right there...
+now with hotkey changing button
 
 
 
--- scrollwheel.py
+++ scrollwheel.py
@@ -6,8 +6,9 @@ import keyboard
 import threading
 
 is_on = False
+myhotkey = "f6"
 
-def scroll():
+def scroll(): # actually do the scrolling
     global is_on
     while is_on:
         mouse.wheel(-1)
@@ -18,18 +19,38 @@ def toggle():
     global is_on
     if not is_on:
         is_on = True
-        threading.Thread(target=scroll, daemon=True).start()
-        print("started")
+        indicator.config(text="on", foreground="green", font=("comic sans", 25))
+        threading.Thread(target=scroll, daemon=True).start() # start scrolling and set the big label to on
     else:
         is_on = False
+        indicator.config(text="off", foreground="red", font=("comic sans", 25)) # stop and set label to off
+
+def set_hotkey():
+    global myhotkey
+    oldkey = myhotkey
+    rkey = keyboard.read_key() # wait for keypress and then continue
+
+    if rkey == 'esc':
         print("stopped")
+        return
+    else:
+        myhotkey = rkey
+        keyboard.remove_hotkey(str(oldkey))
+        keyboard.add_hotkey(rkey, toggle)
+        hotkeybut.config(text=rkey)
+        print(rkey)
 
-keyboard.add_hotkey("f6", toggle)
-
+keyboard.add_hotkey(myhotkey, toggle)
 
 root = Tk()
+root.minsize(200, 100)
+root.title("scrolling thing")
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm,  text="Press F6 to start/stop").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+indicator = ttk.Label(frm, text="off", foreground="red", font=("comic sans", 25))
+indicator.grid(column=0, row=0)
+ttk.Label(frm,  text="button to change hotkey\n(esc to cancel)").grid(column=0, row=1)
+hotkeybut = ttk.Button(frm, text="f6", command=set_hotkey)
+hotkeybut.grid(column=0, row=2)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
 root.mainloop()
